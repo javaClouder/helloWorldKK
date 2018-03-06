5191c6659cf188cd018edd957a3aea30bdddbfe7
Karan Katyal <karankatyal93@gmail.com>
2018-03-06 09:06:37 -0800

    Resotred a file d93b1e9

--- helloWorld.java
+++ helloWorld.java
@@ -1,12 +1,9 @@
-
-//This is the code
+//This java code will print hello world
 public class helloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-	//prints hello from the other side
-	System.out.println("Hello from the other side);
     }
 
 }
