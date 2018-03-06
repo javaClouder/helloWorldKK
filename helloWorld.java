8716e5fe44de8b04bd799241b14cc89bb16a2af0
Karan Katyal <karankatyal93@gmail.com>
2018-03-06 08:50:08 -0800

    Added a comment line telling about the print statement

--- helloWorld.java
+++ helloWorld.java
@@ -5,6 +5,7 @@ public class helloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+	//prints hello from the other side
 	System.out.println("Hello from the other side);
     }
 
