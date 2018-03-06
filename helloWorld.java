a7102b1e0b118afa5c4355dc40a081030ce7cce0
Karan Katyal <karankatyal93@gmail.com>
2018-03-06 07:56:24 -0800

    Added a print statement and changed the comments

--- helloWorld.java
+++ helloWorld.java
@@ -1,9 +1,11 @@
-//This java code will print hello world
+
+//This is the code
 public class helloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+	System.out.println("Hello from the other side);
     }
 
 }
