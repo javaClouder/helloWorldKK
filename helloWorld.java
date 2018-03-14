576ae47f331df457b0c1f5be116f9a42b75f698f
Karan Katyal <karankatyal93@gmail.com>
2018-03-14 12:53:34 -0700

    restored helloWOrld.java from commit 8716e5f

--- helloWorld.java
+++ helloWorld.java
@@ -1,10 +1,12 @@
-//This java code will print hello world
+
+//This is the code
 public class helloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-	System.out.println("Error check");
+	//prints hello from the other side
+	System.out.println("Hello from the other side);
     }
 
 }
