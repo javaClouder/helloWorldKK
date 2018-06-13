5cdc15f60d6bffe0117e0944fcdf09e54e2a31ae
kk katyal <playwaykaran@gmail.com>
2018-06-12 23:59:17 -0400

    added a print statement to test jenkins on ec2

--- helloWorld.java
+++ helloWorld.java
@@ -9,7 +9,8 @@ public class helloWorld {
 	System.out.println("Hello from the other side");
 	System.out.println("Hello Dogs and bitches out there!");
 	System.out.println("The print statement to test Jenkins triggers");
-//	System.out.println("The print statement to test Jenkins triggers again!");
+	System.out.println("The print statement to test Jenkins triggers again!");
+	System.out.println("Tring junkins Master on EC2");
     }
 
 }
